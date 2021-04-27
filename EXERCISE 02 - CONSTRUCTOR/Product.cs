b53e3a1b1cbb27a76eba313bd0fcd3566ba14d3f
Marcus Bianchi <82729031+CmdBianchi@users.noreply.github.com>
2021-04-27 10:09:59 -0300

    Adicionar arquivos de projeto.

---
+++ EXERCISE 02 - CONSTRUCTOR/Product.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace EXERCISE_02___CONSTRUCTOR {
+    //------------------------------- START -------------------------------//
+    class Product {
+        public string Name;
+        public double Price;
+        public int Amount;
+
+        //------------------------- MET. CLASS ----------------------------//
+        public Product( string name, double price, int amount) {
+            Name = name;
+            Price = price;
+            Amount = amount;
+        }
+        public double TotalValueStock() {
+            return Price * Amount;
+        }
+        public void AddProduct(int amount) {
+            Amount += amount;
+        }
+        public void RemoveProduct(int amount) {
+            Amount -= amount;
+        }
+        public override string ToString() {
+            return Name
+                + "| $ " + Price.ToString("F2") + "| " + Amount + "  Unidades | Total: $ " + TotalValueStock().ToString("F2");
+        }
+    }
+    //-------------------------------- END -------------------------------// 
+}
---
+++ EXERCISE 02 - CONSTRUCTOR/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+namespace EXERCISE_02___CONSTRUCTOR {
+    class Program {
+        // -------> START / MAIN 
+        /////////////////////////////////////////////////////////////////////////////////////////////
+        static void Main(string[] args) {
+            Console.Write("Entre os dados do poduto: ");
+            Console.Write("Nome: "); string name= Console.ReadLine();
+            Console.Write("Preço: "); double price = double.Parse(Console.ReadLine());
+            Console.Write("quantidade no estoque: "); int amount = int.Parse(Console.ReadLine());
+
+            Product p = new Product(name, price, amount);//----------------------------------------------------------------> Chamada da classe 
+
+            Console.WriteLine();
+            Console.Write("Dados do produto: "+p);
+
+            Console.WriteLine();
+            Console.Write("Digite o número de produtos a ser adicionado ao estoque: ");
+            int amnt = int.Parse(Console.ReadLine());
+            p.AddProduct(amnt); //----------------------------------------------------------------> Chamada da classe 
+
+            Console.WriteLine();
+            Console.Write("Dados att: " +p);
+
+            Console.WriteLine();
+            Console.Write("Digite o número a ser removido do estoque: ");
+            amount = int.Parse(Console.ReadLine());
+            p.RemoveProduct(amount); //----------------------------------------------------------------> Chamada da classe 
+
+            Console.WriteLine();
+            Console.Write("Dados att: "+p);
+        }
+        /////////////////////////////////////////////////////////////////////////////////////////////
+        // -------> END
+    }
+}
